8374fea37068d37dfbaf840027b4f6ad9ac2d726
Pablo Solano <Pablo61098>
2020-08-25 23:28:18 -0500

    updating app with show-page, collection-preview and collection-item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component.jsx';
+import './App.css';
 
+// const HatsPage = () => (
+//   <div>
+//     <h1>HATS PAGE</h1>
+//   </div>  
+// );
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} ></Route>  
+        <Route  path='/shop' component={ShopPage}></Route>  
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({title, imageUrl, size}) => (
+const MenuItem = ({title, imageUrl, size, linkUrl, match, history}) => (
 
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div style={{
              backgroundImage: `url(${imageUrl})`
             }} 
@@ -17,7 +18,7 @@ const MenuItem = ({title, imageUrl, size}) => (
 
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
 
 
 
